d0a29dc193751410882a9981eb6655b58819b8f0
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2022-04-22 18:54:33 -0300

    refactor: mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,23 +29,21 @@ module.exports = {
     boxShadow: {
       card: "0 20px 44px -16px hsl(218, 22%, 67%)",
       "light-cyan-button":
-        "inset 0 0 2px 1px hsl(179, 62%, 43%), 0 8px 14px -2px rgba(0, 0, 0, 0.15)",
+        "inset 0 0 1px 1px hsl(179, 62%, 43%), 0 8px 14px -2px rgba(0, 0, 0, 0.15)",
       "yellow-button":
-        "inset 0 0 2px 1px hsl(71, 73%, 54%), 0 8px 14px -2px rgba(0, 0, 0, 0.15)",
+        "inset 0 0 1px 1px hsl(71, 73%, 54%), 0 8px 14px -2px rgba(0, 0, 0, 0.15)",
     },
     extend: {
       fontSize: {
         "main-title": ["1.5rem", "2rem"],
-        "tablet-main-title": ["1.21875rem", "1.5rem"],
         "secondary-title": ["1.1875rem", "1.625rem"],
-        "tablet-secondary-title": ["1.0625rem", "1.5rem"],
-        "tablet-subtitle": ["0.875rem", "1.375rem"],
         "strong-subtitle": ["1.125rem", "1.5rem"],
-        "tablet-strong-subtitle": ["0.915625rem", "1rem"],
-        price: ["2rem", "3rem"],
-        "tablet-time-mark": ["0.8125rem", "1rem"],
-        "tablet-left-column": ["0.8125rem", "1rem"],
+        price: ["2rem", "2.9375rem"],
         "right-column": ["0.875rem", "1.28125rem"],
+        "tablet-main-title": ["1.25rem", "1.875rem"],
+        "tablet-secondary-title": ["1.125rem", "1.6875rem"],
+        "tablet-subtitle": ["0.875rem", "1.625rem"],
+        "tablet-strong-subtitle": ["0.9375rem", "1.25rem"],
         "tablet-right-column": ["0.75rem", "1rem"],
       },
       spacing: {
